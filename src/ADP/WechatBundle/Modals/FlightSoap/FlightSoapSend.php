50012d307327dc08cd8c36bc4579fdeda7b64a30
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-24 17:29:28 +0800

    update flight info function1

--- src/ADP/WechatBundle/Modals/FlightSoap/FlightSoapSend.php
+++ src/ADP/WechatBundle/Modals/FlightSoap/FlightSoapSend.php
@@ -6,7 +6,7 @@ require_once dirname(__FILE__).'/FlightSoapResponse.php';
 $FlightSoapResponse = new FlightSoapResponse();
 while($FlightSoapResponse->ststus())
 {
-  $FlightSoapResponse->pushMsg();
+  $FlightSoapResponse->pushSoap();
 }
 
 ?>
