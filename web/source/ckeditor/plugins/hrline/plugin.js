8c689114d04b29992fdc9b337631035a92587ef8
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-27 18:56:15 +0800

    add ckedit button

--- web/source/ckeditor/plugins/hrline/plugin.js
+++ web/source/ckeditor/plugins/hrline/plugin.js
@@ -1,24 +1,24 @@
-CKEDITOR.plugins.add( 'dropmenuckedit',
+CKEDITOR.plugins.add( 'hrline',
     {
         init : function( editor )
         {
 
             // Register the command.
-            editor.addCommand( 'dropmenuckedit',{
+            editor.addCommand( 'hrline',{
                 exec : function( editor )
                 {
-                  var newhtml = CKEDITOR.dom.element.createFromHtml('<div class="dropmenuckedit"><div>title</div><div><div>content</div></div></div>');
+                  var newhtml = CKEDITOR.dom.element.createFromHtml('<hr color="#e63f0c" style="width:60%">');
                     // editor.insertHtml('<div class="dropmenuckedit"><div>aaaaaa</div><div>bbbbbb</div></div>');
                     editor.insertElement(newhtml);
                 }
             });
             // alert('dedephp!');
             // Register the toolbar button.
-            editor.ui.addButton( 'dropmenuckedit',
+            editor.ui.addButton( 'hrline',
             {
-                label : 'dropshow',
-                command : 'dropmenuckedit',
-                icon: 'images/html_add.png'
+                label : 'hrline',
+                command : 'hrline',
+                icon: 'images/page_red.png'
             });
             // alert(editor.name);
         },
